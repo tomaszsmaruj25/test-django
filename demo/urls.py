58f5b666a8a555b89790b77b3d356bcf3d6185c2
tomaszsmaruj25 <tomasz.smaruj@wp.pl>
2022-08-17 16:15:29 +0200

    test app

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,7 @@
-"""djangoProject URL Configuration
+"""demo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('food.urls')),
     path('admin/', admin.site.urls),
 ]
--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,7 @@
+from django.conf.urls import url
 from django.urls import path
+from food import views
 
-from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
